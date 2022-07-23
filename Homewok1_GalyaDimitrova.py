9f6357f4a6e7334908d164c160a4fc86068c1e76
GalyaDimitrovaa <108681042+GalyaDimitrovaa@users.noreply.github.com>
2022-07-23 17:34:25 +0300

    Update Homewok1_GalyaDimitrova.py

--- Homewok1_GalyaDimitrova.py
+++ Homewok1_GalyaDimitrova.py
@@ -39,6 +39,29 @@ print(fib_number(3))
 print(fib_number(10))
 
 
+# zadacha 5
+print("primer 5")
+def is_number_balanced(number):
+    digits = str(number)
+    digits_count = len(digits)
+    sum_left = 0
+    sum_right = 0
+
+    for i in range(digits_count // 2):
+        sum_left += int(digits[i])
+        sum_right += int(digits[digits_count - 1 - i])
+    
+    if sum_left == sum_right:
+        return True
+    else:
+        return False
+
+print(is_number_balanced(9))
+print(is_number_balanced(1238033))
+print(is_number_balanced(28471))
+print(is_number_balanced(4518))
+
+
 
 
 
